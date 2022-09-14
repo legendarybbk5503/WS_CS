9112e413396a60cdfdf099e548684763a8e84661
legendarybbk#5503 <72668615+legendarybbk5503@users.noreply.github.com>
2022-09-15 01:02:58 +0800

    update

--- Unit 1/1.4 HW_3.py
+++ Unit 1/1.4 HW_3.py
@@ -5,11 +5,14 @@ def newResult():
     while True:
         choice = int(input("Enter corresponding number for school: "))
         if choice == -1: break
-        medal[choice-1] += 1
+        elif choice in range(1, len(school)+1):
+            medal[choice-1] += 1
+        else:
+            print("Invalid school number")
 
 def output():
     for i in range(len(school)):
-        print(f"School number {i}   School name {school[i]}   Number of medals {medal[i]}")
+        print(f"School number {i+1}   School name {school[i]}   Number of medals {medal[i]}")
 
 def main():
     print("1: AAAA")
