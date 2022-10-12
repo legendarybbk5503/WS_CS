62d1e6dbc433f2bdc611398c83fbd7a13520248e
legendarybbk#5503 <72668615+legendarybbk5503@users.noreply.github.com>
2022-10-12 18:41:58 +0800

    Update soduku solver.py

--- independent study/soduku solver.py
+++ independent study/soduku solver.py
@@ -16,8 +16,6 @@ def sudoku(puzzle):
             if not(con1 or con2 or con3):
                 trial_list.append(trial)
         return trial_list
-    
-    #trial(7, 7)
 
     while True:
         for i in range(9):
@@ -34,20 +32,19 @@ def sudoku(puzzle):
         for _list in puzzle:
             count += _list.count(0)
         if count == 0: break
-
-
-    """return the solved puzzle as a 2d array of 9 x 9"""
     return puzzle
 
-puzzle = [[5,3,0,0,7,0,0,0,0],
-          [6,0,0,1,9,5,0,0,0],
-          [0,9,8,0,0,0,0,6,0],
-          [8,0,0,0,6,0,0,0,3],
-          [4,0,0,8,0,3,0,0,1],
-          [7,0,0,0,2,0,0,0,6],
-          [0,6,0,0,0,0,2,8,0],
-          [0,0,0,4,1,9,0,0,5],
-          [0,0,0,0,8,0,0,7,9]]
+puzzle = [
+[5,3,0,0,7,0,0,0,0],
+[6,0,0,1,9,5,0,0,0],
+[0,9,8,0,0,0,0,6,0],
+[8,0,0,0,6,0,0,0,3],
+[4,0,0,8,0,3,0,0,1],
+[7,0,0,0,2,0,0,0,6],
+[0,6,0,0,0,0,2,8,0],
+[0,0,0,4,1,9,0,0,5],
+[0,0,0,0,8,0,0,7,9]
+]
 
 def main():
     sudoku(puzzle)
