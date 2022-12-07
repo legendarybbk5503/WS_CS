6571890ac43953d3d35faccad28073921fe1dc8e
legendarybbk#5503 <72668615+legendarybbk5503@users.noreply.github.com>
2022-12-07 09:25:37 +0000

    Update sorting.py

--- independent study/sorting.py
+++ independent study/sorting.py
@@ -2,7 +2,7 @@ class Sort():
     def __init__(self, __list):
         self.__list = __list
 
-    def InsertionSort(self):
+    def insertionSort(self):
         for i in range(1, len(self.__list)):
             now, pos = self.__list[i], i
             while pos > 0 and self.__list[pos-1] > now:
@@ -11,14 +11,14 @@ class Sort():
             self.__list[pos] = now
         return self.__list
 
-    def BubbleSort(self):
+    def bubbleSort(self):
         for _ in range(len(self.__list)-1):
             for j in range(len(self.__list)-1):
                 if self.__list[j] > self.__list[j+1]:
                     self.__list[j], self.__list[j+1] = self.__list[j+1], self.__list[j]
         return self.__list
 
-    def MergeSort(self, _list = None):
+    def mergeSort(self, _list = None):
         if _list is None: _list = self.__list
         if len(_list) > 1:
             mid = len(_list) // 2
@@ -51,7 +51,7 @@ class Sort():
         self.__list = _list
         return self.__list
 
-    def BinarySearch(self, x):
+    def binarySearch(self, x):
         _list, i, j = self.__list, 0, len(self.__list)-1
         while True:
             if _list[(i+j)//2] == x: return (i+j)//2
@@ -59,18 +59,18 @@ class Sort():
             elif _list[(i+j)//2] > x: j = (i+j)//2-1
             elif _list[(i+j)//2] < x: i = (i+j)//2+1
 
-    def LinearSearch(self, x):
+    def linearSearch(self, x):
         i = [i for i in range(len(self.__list)) if self.__list[i] == x]
         return -1 if not len(i) else i[0]
 
 def main():
     _list = [15, 73, 29, 66, 35, 11, 43, 21]
     c = Sort(_list)
-    print(c.InsertionSort())
-    print(c.BubbleSort())
-    print(c.MergeSort())
-    print(c.BinarySearch(66))
-    print(c.LinearSearch(67))
+    print(c.insertionSort())
+    print(c.bubbleSort())
+    print(c.mergeSort())
+    print(c.binarySearch(66))
+    print(c.linearSearch(67))
 
 if __name__ == "__main__":
     main()
